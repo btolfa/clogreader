ae9d7cd71f9fdc756cc53f244fd78f4a9ac7238e
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-06 21:18:17 +0300

    Реализовал main для консольного приложения

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,13 +1,36 @@
 #include "../logreader/CLogReader.h"
 
-#include <iostream>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
-	if (argc == 1)
+	if (argc < 3)
 	{
-		printf("Usage: clogreader.exe PATTERN FILENAME\n");
+		printf("Usage\n");
+		printf("\tclogreader PATTERN FILENAME\n");
+		printf("Example:\n");
+		printf("\tclogreader \"order*closed\" test.log\n");
 	} else {
 		CLogReader reader;
+
+		auto filename = argv[2];
+		auto pattern = argv[1];
+
+		if (! reader.Open(filename)) {
+			printf("Error opening file: %s\n", filename);
+			return 1;
+		}
+
+		if (! reader.SetFilter(pattern, strlen(pattern))) {
+			printf("Error with pattern: %s\n", pattern);
+		}
+
+		char buffer[4096];
+		while (reader.GetNextLine(buffer, sizeof(buffer))) {
+			printf("%s", buffer);
+		}
+
+		printf("\n");
+		reader.Close();
 	}
 
 	
